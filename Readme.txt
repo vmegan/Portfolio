bd74c804624e9ff4942d78798e9b60fb99194992
Meghana Bowen <vmeghana86@gmail.com>
2017-01-10 21:58:22 -0500

    initial commit

---
+++ Readme.txt
@@ -0,0 +1,23 @@
+----------------------------------
+Me - Responsive Personal Portfolio
+----------------------------------
+
+Design & Code by Guglielmo Pardo
+
+WebSite: guglielmopardo.me
+Twitter: @thegixx
+CreativeMarket: creativemarket.com/guglielmo
+
+Please feel free to contact me if you have any questions or concerns.
+
+
+_________________________________
+_________________________________
+
+
+Contact Form Set-Up
+
+1. Write your email in the 'contact/contactengine.php' file, where indicated.
+2. Upload it to your server and enjoy ;)
+
+_________________________________
---
+++ contact/contactengine.php
@@ -0,0 +1,35 @@
+<?php
+
+$EmailFrom = Trim(stripslashes($_POST['Email']));
+$EmailTo = "youremail@mail.com"; // <- Please add your email
+$Subject = "Message from your WebSite contact form";
+$Title = Trim(stripslashes($_POST['Name'])); 
+$Message = Trim(stripslashes($_POST['Message'])); 
+
+// validation
+$validationOK=true;
+if (!$validationOK) {
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
+  exit;
+}
+
+// prepare email body text
+$Body = "";
+$Body .= "Name: ";
+$Body .= $Name;
+$Body .= "\n";
+$Body .= "Message: ";
+$Body .= $Message;
+$Body .= "\n";
+
+// send email 
+$success = mail($EmailTo, $Subject, $Body, "From: <$EmailFrom>");
+
+// redirect to success page 
+if ($success){
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=contactthanks.php\">";
+}
+else{
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
+}
+?>
